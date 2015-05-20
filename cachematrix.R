0c3b3855c92c04ea24dd2c1f144191ee371d2334
smaggie <maitreyi.soman@gmail.com>
2015-05-20 10:58:50 -0400

    Programming Assignment 2
    Create a matrix and calculate inverse for the matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,21 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is a costly computation, and hence there are benefits to caching the inverse
+## of a matrix.
+## The following two functions will help in creating a special "matrix" object, and help compute
+## the inverse of this "matrix" object.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a list containing a function to
+## 1. set the value of the matrix
+## 2. get the value of the matrix
+## 3. set the value of inverse of the matrix
+## 4. get the value of inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    
+    set <- function(y){
+        x <<- y
+        m <<- NULL
+    }
 }
 
 
